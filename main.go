ad9ebd0bc45ff87ac08a7536180bd69786865885
riversteve <esswein93@gmail.com>
2021-01-22 22:34:24 +0000

    Regex doesnt need numbers in users

--- main.go
+++ main.go
@@ -1,33 +1,33 @@
 package main
 
 import (
-    "net/http"
-    "log"
+	"log"
+	"net/http"
 
-    "github.com/gorilla/mux"
-    _ "github.com/mattn/go-sqlite3"
+	"github.com/gorilla/mux"
+	_ "github.com/mattn/go-sqlite3"
 )
 
 type stat struct {
-	Player     string `json_object:"player_id"`
-	Date       string `json:"date_key"`
-	Mode       string `json:"game_mode_sub"`
-	Kills      string `json:"kills"`
+	Player string `json_object:"player_id"`
+	Date   string `json:"date_key"`
+	Mode   string `json:"game_mode_sub"`
+	Kills  string `json:"kills"`
 }
 
 func main() {
-    r := mux.NewRouter()
+	r := mux.NewRouter()
 
-    api := r.PathPrefix("/api").Subrouter()
-    api.HandleFunc("/", get).Methods(http.MethodGet)
-    api.HandleFunc("", get).Methods(http.MethodGet)
-    api.HandleFunc("", post).Methods(http.MethodPost)
-    api.HandleFunc("", put).Methods(http.MethodPut)
-    api.HandleFunc("", delete).Methods(http.MethodDelete)
-    api.HandleFunc("/kills", mostKills).Methods(http.MethodGet)
-    api.HandleFunc("/pr/{userName:[a-z0-9_]{3,10}}/kills", prKills).Methods(http.MethodGet)
-    //api.HandleFunc("/pr/{userName:[a-z0-9_]{3,10}}/kd", prKD).Methods(http.MethodGet)
-    api.HandleFunc("/user/{userID}/comment/{commentID}", params).Methods(http.MethodGet)
+	api := r.PathPrefix("/api").Subrouter()
+	api.HandleFunc("/", get).Methods(http.MethodGet)
+	api.HandleFunc("", get).Methods(http.MethodGet)
+	api.HandleFunc("", post).Methods(http.MethodPost)
+	api.HandleFunc("", put).Methods(http.MethodPut)
+	api.HandleFunc("", delete).Methods(http.MethodDelete)
+	api.HandleFunc("/kills", mostKills).Methods(http.MethodGet)
+	api.HandleFunc("/pr/{userName:[a-z_]{3,10}}/kills", prKills).Methods(http.MethodGet)
+	//api.HandleFunc("/pr/{userName:[a-z_]{3,10}}/kd", prKD).Methods(http.MethodGet)
+	api.HandleFunc("/user/{userID}/comment/{commentID}", params).Methods(http.MethodGet)
 
-    log.Fatal(http.ListenAndServe(":9990", r))
+	log.Fatal(http.ListenAndServe(":9990", r))
 }
